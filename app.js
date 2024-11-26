686f58cdfe5345a13d4c5eb1b82c44608833af54
Ayaka Ito <a.8.32.noko.info@gmail.com>
2024-11-26 11:52:30 -0800

    modified:   app.js

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 // COMPLETE THE CODE
 
-$(function() {
-    // Exercise 1: Find all <li> elements within a <ul> and change their text color to blue
-    $("#exercise1Button").on('click', function() {
-      $("#exercise1 ul").
-    });
+$(function () {
+  // Exercise 1: Find all <li> elements within a <ul> and change their text color to blue
+  $("#exercise1Button").on("click", function () {
+    $("#exercise1 ul").find("li").css("color", "blue");
+  });
 
-    // Exercise 2: Find the parent <div> of a paragraph and change its background color
-    $("#exercise2Button").on('click', function() {
-      $("#exercise2 p").
-    });
+  // // Exercise 2: Find the parent <div> of a paragraph and change its background color
+  $("#exercise2Button").on("click", function () {
+    $("#exercise2 p").parent().css("background-color", "pink");
+  });
 
-    // Exercise 3: Find all ancestor <div> elements of a paragraph and change their background color
-    $("#exercise3Button").on('click', function() {
-      $("#exercise3 p").
-    });
+  // // Exercise 3: Find all ancestor <div> elements of a paragraph and change their background color
+  $("#exercise3Button").on("click", function () {
+    $("#exercise3 p").parents("div").css("background-color", "green");
+  });
 
-    // Exercise 4: From the `this` selector, find and hide only the second <p> sibling of the <h3> element. The `this` keyword refers to the clicked button
-    $("#exercise4Button").on('click', function() {
-      $(this).
-    });
+  // // Exercise 4: From the `this` selector, find and hide only the second <p> sibling of the <h3> element. The `this` keyword refers to the clicked button
+  $("#exercise4Button").on("click", function () {
+    $(this).siblings("h3").next().next().hide();
+  });
 
-    // Exercise 5: From the `this` selector, find all <p> elements within the <div> and add a border to them. The `this` keyword refers to the clicked button
-    $("#exercise5Button").on('click', function() {
-      $(this).
-    });
+  // // Exercise 5: From the `this` selector, find all <p> elements within the <div> and add a border to them. The `this` keyword refers to the clicked button
+  $("#exercise5Button").on("click", function () {
+    $(this).next("div").find("p").css("border", "2px solid black");
+  });
 
-    // Exercise 6: From the `this` selector, find and change the color of <span>Item 5</span>. The `this` keyword refers to the clicked button
-    $("#exercise6Button").on('click', function() {
-      $(this).
-    });
-})
+  // // Exercise 6: From the `this` selector, find and change the color of <span>Item 5</span>. The `this` keyword refers to the clicked button
+  $("#exercise6Button").on("click", function () {
+    $(this).prev().prev().find("span").css("color", "red");
+  });
+});
